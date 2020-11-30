c8b03ccdb3e8410ce2b7b2a7add209aabfb642bb
Dmitry Lyukov <d@lyukov.com>
2020-11-30 04:53:54 +0300

    Fix parameters in run scripts

--- bluegene/run.sh
+++ bluegene/run.sh
@@ -2,33 +2,33 @@
 
 for N in 128 256 512
 do
-  mpisubmit.bg -n 1   -w 00:15:00 -m smp wave -- 1 0.05 $N 20 1 1 1
-  mpisubmit.bg -n 64  -w 00:15:00 -m smp wave -- 1 0.05 $N 20 4 4 4
-  mpisubmit.bg -n 128 -w 00:15:00 -m smp wave -- 1 0.05 $N 20 8 4 4
-  mpisubmit.bg -n 256 -w 00:10:00 -m smp wave -- 1 0.05 $N 20 8 8 4
+  mpisubmit.bg -n 1   -w 00:15:00 -m smp wave -- 1 0.01 $N 20 1 1 1
+  mpisubmit.bg -n 64  -w 00:15:00 -m smp wave -- 1 0.01 $N 20 4 4 4
+  mpisubmit.bg -n 128 -w 00:15:00 -m smp wave -- 1 0.01 $N 20 8 4 4
+  mpisubmit.bg -n 256 -w 00:10:00 -m smp wave -- 1 0.01 $N 20 8 8 4
 done
 
 for N in 128 256 512
 do
-  mpisubmit.bg -n 1   -w 00:15:00 -m smp wavePi -- 3.14159 0.05 $N 20 1 1 1
-  mpisubmit.bg -n 64  -w 00:15:00 -m smp wavePi -- 3.14159 0.05 $N 20 4 4 4
-  mpisubmit.bg -n 128 -w 00:15:00 -m smp wavePi -- 3.14159 0.05 $N 20 8 4 4
-  mpisubmit.bg -n 256 -w 00:10:00 -m smp wavePi -- 3.14159 0.05 $N 20 8 8 4
+  mpisubmit.bg -n 1   -w 00:15:00 -m smp wavePi -- 3.14159 0.01 $N 20 1 1 1
+  mpisubmit.bg -n 64  -w 00:15:00 -m smp wavePi -- 3.14159 0.01 $N 20 4 4 4
+  mpisubmit.bg -n 128 -w 00:15:00 -m smp wavePi -- 3.14159 0.01 $N 20 8 4 4
+  mpisubmit.bg -n 256 -w 00:10:00 -m smp wavePi -- 3.14159 0.01 $N 20 8 8 4
 done
 
 for N in 128 256 512
 do
-  mpisubmit.bg -n 1   -w 00:15:00 -m smp -e "OMP_NUM_THREADS=2" wavePi_omp -- 3.14159 0.05 $N 20 1 1 1
-  mpisubmit.bg -n 64  -w 00:15:00 -m smp -e "OMP_NUM_THREADS=2" wavePi_omp -- 3.14159 0.05 $N 20 4 4 4
-  mpisubmit.bg -n 128 -w 00:15:00 -m smp -e "OMP_NUM_THREADS=2" wavePi_omp -- 3.14159 0.05 $N 20 8 4 4
-  mpisubmit.bg -n 256 -w 00:10:00 -m smp -e "OMP_NUM_THREADS=2" wavePi_omp -- 3.14159 0.05 $N 20 8 8 4
+  mpisubmit.bg -n 1   -w 00:15:00 -m smp -e "OMP_NUM_THREADS=2" wavePi_omp -- 3.14159 0.01 $N 20 1 1 1
+  mpisubmit.bg -n 64  -w 00:15:00 -m smp -e "OMP_NUM_THREADS=2" wavePi_omp -- 3.14159 0.01 $N 20 4 4 4
+  mpisubmit.bg -n 128 -w 00:15:00 -m smp -e "OMP_NUM_THREADS=2" wavePi_omp -- 3.14159 0.01 $N 20 8 4 4
+  mpisubmit.bg -n 256 -w 00:10:00 -m smp -e "OMP_NUM_THREADS=2" wavePi_omp -- 3.14159 0.01 $N 20 8 8 4
 done
 
 for N in 128 256 512
 do
-  mpisubmit.bg -n 1   -w 00:15:00 -m smp -e "OMP_NUM_THREADS=2" wave_omp -- 1 0.05 $N 20 1 1 1
-  mpisubmit.bg -n 64  -w 00:15:00 -m smp -e "OMP_NUM_THREADS=2" wave_omp -- 1 0.05 $N 20 4 4 4
-  mpisubmit.bg -n 128 -w 00:15:00 -m smp -e "OMP_NUM_THREADS=2" wave_omp -- 1 0.05 $N 20 8 4 4
-  mpisubmit.bg -n 256 -w 00:10:00 -m smp -e "OMP_NUM_THREADS=2" wave_omp -- 1 0.05 $N 20 8 8 4
+  mpisubmit.bg -n 1   -w 00:15:00 -m smp -e "OMP_NUM_THREADS=2" wave_omp -- 1 0.01 $N 20 1 1 1
+  mpisubmit.bg -n 64  -w 00:15:00 -m smp -e "OMP_NUM_THREADS=2" wave_omp -- 1 0.01 $N 20 4 4 4
+  mpisubmit.bg -n 128 -w 00:15:00 -m smp -e "OMP_NUM_THREADS=2" wave_omp -- 1 0.01 $N 20 8 4 4
+  mpisubmit.bg -n 256 -w 00:10:00 -m smp -e "OMP_NUM_THREADS=2" wave_omp -- 1 0.01 $N 20 8 8 4
 done
 
--- polus/run.sh
+++ polus/run.sh
@@ -2,16 +2,16 @@
 
 for N in 128 256 512
 do
-  mpisubmit.pl -p 1  -w 00:15 wave -- 1 0.05 $N 20 1 1 1
-  mpisubmit.pl -p 10 -w 00:15 wave -- 1 0.05 $N 20 5 2 1
-  mpisubmit.pl -p 20 -w 00:15 wave -- 1 0.05 $N 20 5 2 2
-  mpisubmit.pl -p 40 -w 00:15 wave -- 1 0.05 $N 20 5 4 2
+  mpisubmit.pl -p 1  -w 00:15 wave -- 1 0.01 $N 20 1 1 1
+  mpisubmit.pl -p 10 -w 00:15 wave -- 1 0.01 $N 20 5 2 1
+  mpisubmit.pl -p 20 -w 00:15 wave -- 1 0.01 $N 20 5 2 2
+  mpisubmit.pl -p 40 -w 00:15 wave -- 1 0.01 $N 20 5 4 2
 done
 
 for N in 128 256 512
 do
-  mpisubmit.pl -p 1  -w 00:15 wavePi -- 3.14159 0.1 $N 20 1 1 1
-  mpisubmit.pl -p 10 -w 00:15 wavePi -- 3.14159 0.1 $N 20 5 2 1
-  mpisubmit.pl -p 20 -w 00:15 wavePi -- 3.14159 0.1 $N 20 5 2 2
-  mpisubmit.pl -p 40 -w 00:15 wavePi -- 3.14159 0.1 $N 20 5 4 2
+  mpisubmit.pl -p 1  -w 00:15 wavePi -- 3.14159 0.01 $N 20 1 1 1
+  mpisubmit.pl -p 10 -w 00:15 wavePi -- 3.14159 0.01 $N 20 5 2 1
+  mpisubmit.pl -p 20 -w 00:15 wavePi -- 3.14159 0.01 $N 20 5 2 2
+  mpisubmit.pl -p 40 -w 00:15 wavePi -- 3.14159 0.01 $N 20 5 4 2
 done
