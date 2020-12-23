31625e73368cf66f8831c9da0ed3582b9de9f061
Dmitry Lyukov <d@lyukov.com>
2020-12-24 02:30:57 +0300

    Use raw indexing

--- src/CudaSolver.cu
+++ src/CudaSolver.cu
@@ -31,16 +31,11 @@ __constant__ double d_h_z;
 __constant__ double d_sqr_tau;
 
 __device__
-double &get_at(double *g, int i, int j, int k) {
-    return g[i * d_cfI + j * d_cfJ + k];
-}
-
-__device__
-double laplacian(double *g, int i, int j, int k) {
-    double center = get_at(g, i, j, k);
-    return (get_at(g, i - 1, j, k) - 2.0 * center + get_at(g, i + 1, j, k)) / (d_h_x * d_h_x) +
-           (get_at(g, i, j - 1, k) - 2.0 * center + get_at(g, i, j + 1, k)) / (d_h_y * d_h_y) +
-           (get_at(g, i, j, k - 1) - 2.0 * center + get_at(g, i, j, k + 1)) / (d_h_z * d_h_z);
+double laplacian(double *g, int index) {
+    double center = g[index];
+    return (g[index - d_cfI] - 2.0 * center + g[index + d_cfI]) / (d_h_x * d_h_x) +
+           (g[index - d_cfJ] - 2.0 * center + g[index + d_cfJ]) / (d_h_y * d_h_y) +
+           (g[index - 1] - 2.0 * center + g[index + 1]) / (d_h_z * d_h_z);
 }
 
 __global__
@@ -48,8 +43,9 @@ void step(double *grid, double *previous_1, double *previous_2) {
     int i = blockIdx.x * blockDim.x + threadIdx.x + 1;
     int j = blockIdx.y * blockDim.y + threadIdx.y + 1;
     int k = blockIdx.z * blockDim.z + threadIdx.z + 1;
-    get_at(grid, i, j, k) = 2.0 * get_at(previous_1, i, j, k) - get_at(previous_2, i, j, k) +
-                            d_sqr_tau * laplacian(previous_1, i, j, k);
+    int index = i * d_cfI + j * d_cfJ + k;
+    grid[index] = 2.0 * previous_1[index] - previous_2[index] +
+                  d_sqr_tau * laplacian(previous_1, index);
 }
 
 void makeStepWithCuda(Grid3D &grid, Grid3D &previous_1, Grid3D &previous_2,
@@ -88,7 +84,7 @@ void makeStepWithCuda(Grid3D &grid, Grid3D &previous_1, Grid3D &previous_2,
 
     SAFE_CALL(cudaMemcpy(grid.getFlatten().data(), d_grid, sizeInBytes, cudaMemcpyDeviceToHost));
 
-//    SAFE_CALL(cudaFree(d_grid));
-//    SAFE_CALL(cudaFree(d_previous_1));
-//    SAFE_CALL(cudaFree(d_previous_2));
+    SAFE_CALL(cudaFree(d_grid));
+    SAFE_CALL(cudaFree(d_previous_1));
+    SAFE_CALL(cudaFree(d_previous_2));
 }
