bccd06e07b53fb20989750d98a6194b8213b9ab8
Dmitry Lyukov <d@lyukov.com>
2020-12-25 04:40:34 +0300

    Remove gprof flags

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 module load SpectrumMPI
-nvcc -ccbin mpicxx -std=c++11 -O3 -Xcompiler "-g -pg" -gencode arch=compute_52,code=sm_52 -gencode arch=compute_60,code=sm_60 src/*.cu src/*.cpp -o cuda_wave
+nvcc -ccbin mpicxx -std=c++11 -O3 -gencode arch=compute_52,code=sm_52 -gencode arch=compute_60,code=sm_60 src/*.cu src/*.cpp -o cuda_wave
