5c81cc78c3542dfb2f07b736fa055b9ac880e068
Dmitry Lyukov <d@lyukov.com>
2020-12-29 00:53:44 +0300

    Delete solver

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 module load SpectrumMPI
 module load OpenMPI
-nvcc -ccbin mpixlC -O3 -std=c++11 src/*.cu src/*.cpp -o cuda_wave
+nvcc -ccbin mpicxx -O3 -std=c++11 src/*.cu src/*.cpp -o cuda_wave
--- build_all.sh
+++ build_all.sh
@@ -1,5 +1,5 @@
 module load SpectrumMPI
 module load OpenMPI
-nvcc -ccbin mpixlC -O3 -std=c++11 src/*.cu src/*.cpp -o cuda_wave
+nvcc -ccbin mpicxx -O3 -std=c++11 src/*.cu src/*.cpp -o cuda_wave
 mpixlC -std=c++11 -qsmp=omp src/*.cpp -o omp_wave
 mpixlC -std=c++11 src/*.cpp -o simple_wave
