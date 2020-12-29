be79233aa344ac984c3c2ebf74e93c0273a02abd
Dmitry Lyukov <d@lyukov.com>
2020-12-29 18:35:08 +0300

    10 threads

--- run_omp.sh
+++ run_omp.sh
@@ -7,7 +7,7 @@ do
     do
         for P in 1 2 3 4
         do
-        export OMP_NUM_THREADS=8
+        export OMP_NUM_THREADS=10
         bsub -q normal -W 00:15 -x -R "span[ptile=1]" -n $P \
              -oo log/omp/${L}_${N}_${P}.out \
              -eo log/omp/${L}_${N}_${P}.err \
